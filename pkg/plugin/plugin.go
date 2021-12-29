8e184a029b277d53c1acb0cb000771db160c48a7
Ole Claussen <ole.claussen@wabenet.com>
2021-12-29 11:56:07 +0000

    fix: handle nil in plugin error

--- pkg/plugin/plugin.go
+++ pkg/plugin/plugin.go
@@ -67,6 +67,10 @@ type ErrInvalidPlugin struct {
 }
 
 func (e ErrInvalidPlugin) Error() string {
+	if e.Plugin == nil {
+		return fmt.Sprintf("invalid unknown plugin encountered: %s", e.Message)
+	}
+
 	return fmt.Sprintf(
 		"invalid plugin '%s' of type %s: %s",
 		e.Plugin.Name,
